8977e461731ec76529f5bcec867ff9dcbed790c7
Bill Janssen <4193527+wjanssen@users.noreply.github.com>
2021-01-11 17:27:57 -0800

    handle missing coverimage

--- builder.py
+++ builder.py
@@ -17,7 +17,7 @@ HEADER1="""
 %% twoside -- printed for binding, openany -- don't wait for an even page to start new article
 \\documentclass[12pt,journal,indent,openany]{paper}
 %% Use paper size good for tablets, recommended in LaTeX book, with 1 inch margins
-\\usepackage[paperwidth=6.125in, paperheight=9.25in]{geometry}
+\\usepackage[paperwidth=7in, paperheight=10in]{geometry}
 %% English main language
 \\usepackage[english]{babel}
 \\usepackage{microtype}
@@ -35,7 +35,7 @@ HEADER1="""
 \\usepackage[most]{tcolorbox}
 """
 
-COVERIMAGE="""
+COVERIMAGE1="""
 %% for transparency
 \\usepackage{transparent}
 %% for images
@@ -56,8 +56,14 @@ HEADER2="""
 %% End of preamble
 %%
 \\begin{document}
+"""
+
+COVERIMAGE2="""
 %% Add background picture on title page
-\AddToShipoutPicture*{\BackgroundPic}
+\\AddToShipoutPicture*{\\BackgroundPic}
+"""
+
+HEADER3="""
 \\begin{titlepage}
 \\begin{tcolorbox}
 \\begin{center}
@@ -155,9 +161,9 @@ def build(args):
     pdffile = args['output']
     contribs_dir = args['contribs']
     coverimagefile = args['coverart']
-    if coverimagefile is not None and not os.path.exists(coverimagefile):
-        raise Exception("Specified cover image " + coverimagefile + " does not exist.")
-    else:
+    if coverimagefile is not None:
+        if not os.path.exists(coverimagefile):
+            raise Exception("Specified cover image " + coverimagefile + " does not exist.")
         args['coverimage'] = os.path.abspath(coverimagefile)
     if not os.path.exists(db):
         raise Exception("No database file; should be " + db)
@@ -187,7 +193,7 @@ def build(args):
             print("converted " + tempfilename + " with status " + str(status))
             if status != 0:
                 raise Exception("Can't convert " + fullfilename + " to latex")
-            tempfiles[row['index']] = {'articleindex': row['index'], 'author': row['author'], 'title': row['title'], 'email': row['email'], 'filename': tempfilename}
+            tempfiles[row['index']] = {'articleindex': row['index'], 'type': row['type'], 'author': row['author'], 'title': row['title'], 'email': row['email'], 'filename': tempfilename}
 
     print(tempfiles)
 
@@ -196,16 +202,28 @@ def build(args):
         args['thisyear'] = datetime.datetime.now().year
         outputfile.write(HEADER1 % args)
         if coverimagefile is not None:
-            outputfile.write(COVERIMAGE % args)
+            outputfile.write(COVERIMAGE1 % args)
         outputfile.write(HEADER2 % args)
+        if coverimagefile is not None:
+            outputfile.write(COVERIMAGE2 % args)
+        outputfile.write(HEADER3 % args)
         outputfile.write(MASTHEAD % args)
         outputfile.write(CONTENTSPAGE)
         outputfile.write(CONTRIBUTIONS_HEADER)
         for article in sorted(tempfiles):
             metadata = tempfiles[article]
+            if metadata['type'] not in ('story', 'poem'):
+                continue
             metadata.update(args)
             outputfile.write(CONTRIBUTION % metadata)
-            outputfile.write(open(metadata['filename']).read())
+            if metadata['type'] == 'story':
+                outputfile.write(open(metadata['filename']).read())
+            elif metadata['type'] == 'poem':
+                for line in open(metadata['filename']).readlines():
+                    if line.strip():
+                        outputfile.write("\\noindent " + line)
+                    else:
+                        outputfile.write(line)
         outputfile.write(FOOTER)
         outputfile.flush()
         # figure out the TeX jobname
@@ -228,11 +246,11 @@ if __name__ == "__main__":
     parser.add_argument("--dbfile", required=True, help="required, specify spreadsheet file (.CSV format) or DB file to pull metadata from")
     parser.add_argument("--contribs", required=True, help="required, specify directory where contributions are, one per file, DOCX or RTF format")
     parser.add_argument("--output", required=True, help="required, specify PDF output file name")
+    parser.add_argument("--issuedate", required=True, help="required, specify date string to use for issue")
+    parser.add_argument("--volume", required=True, help="required, which volume of the magazine is it")
+    parser.add_argument("--number", required=True, help="required, which number of the volume is it")
     parser.add_argument("--coverart", help="specify file containing cover art, if it exists")
     parser.add_argument("--covercredit", help="who gets credit for the cover art")
-    parser.add_argument("--issuedate", help="specify date string to use for issue")
-    parser.add_argument("--volume", help="which volume of the magazine is it")
-    parser.add_argument("--number", help="which number of the volume is it")
     args = parser.parse_args()
     try:
         build(vars(args))
